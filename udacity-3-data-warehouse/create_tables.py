35268ac64d97adf2432049e86b2aef200f71e25e
Jacob B. Madsen <jacob@jbmadsen.com>
2020-02-28 09:27:25 +0100

    Created sql_queries code (untested), and added docstrings to create_tables.py - tested creation/deletion

--- udacity-3-data-warehouse/create_tables.py
+++ udacity-3-data-warehouse/create_tables.py
@@ -6,28 +6,49 @@ from sql_queries import create_table_queries, drop_table_queries
 
 
 def drop_tables(cur, conn):
+    """
+    Drops the tables described by the drop_table_queries list of queries loaded from sql_queries.py
+    
+    Input:
+    conn:   The connection to the database
+    cur:    Cursor object created by the database connection
+    """
+    print("Dropping tables")
+
     for query in drop_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def create_tables(cur, conn):
+    """
+    Creates the tables described by the create_table_queries list of queries loaded from sql_queries.py
+    
+    Input:
+    conn:   The connection to the database
+    cur:    Cursor object created by the database connection
+    """
+    print("Creating tables")
+
     for query in create_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def main():
+    """Creates the database, and the staging, fact and dinmensional tables within the database"""
     config = configparser.ConfigParser()
-    config.read('dwh.cfg')
-
+    config.read('./dwh.cfg')
+    
     conn = psycopg2.connect("host={} dbname={} user={} password={} port={}".format(*config['CLUSTER'].values()))
     cur = conn.cursor()
+    print("Connected to Redshift cluster")
 
     drop_tables(cur, conn)
     create_tables(cur, conn)
 
     conn.close()
+    print("Disconnected from Redshift cluster")
 
 
 if __name__ == "__main__":
--- udacity-3-data-warehouse/sql_queries.py
+++ udacity-3-data-warehouse/sql_queries.py
@@ -1,67 +1,203 @@
+import os, sys
 import configparser
+
+os.chdir(os.path.dirname(sys.argv[0]))
 
 
 # CONFIG
 config = configparser.ConfigParser()
-config.read('dwh.cfg')
+config.read('./dwh.cfg')
+print("Config sections loaded:", config.sections())
+
 
 # DROP TABLES
 
-staging_events_table_drop = ""
-staging_songs_table_drop = ""
-songplay_table_drop = ""
-user_table_drop = ""
-song_table_drop = ""
-artist_table_drop = ""
-time_table_drop = ""
+staging_events_table_drop = "DROP TABLE IF EXISTS staging_events;"
+staging_songs_table_drop = "DROP TABLE IF EXISTS staging_songs;"
+songplay_table_drop = "DROP TABLE IF EXISTS songplays;"
+user_table_drop = "DROP TABLE IF EXISTS users;"
+song_table_drop = "DROP TABLE IF EXISTS songs;"
+artist_table_drop = "DROP TABLE IF EXISTS artists;"
+time_table_drop = "DROP TABLE IF EXISTS time;"
+
 
 # CREATE TABLES
 
 staging_events_table_create= ("""
+CREATE TABLE IF NOT EXISTS staging_events (
+    artist VARCHAR,
+    auth VARCHAR,
+    firstName VARCHAR,
+    gender CHAR,
+    itemInSession INT,
+    lastName VARCHAR,
+    length FLOAT,
+    level VARCHAR,
+    location VARCHAR,
+    method VARCHAR,
+    page VARCHAR,
+    registration FLOAT,
+    sessionId INT,
+    song VARCHAR,
+    status INT,
+    ts BIGINT,
+    userAgent VARCHAR,
+    userId INT
+);
 """)
 
 staging_songs_table_create = ("""
+CREATE TABLE IF NOT EXISTS staging_songs (
+    num_songs INT,
+    artist_id VARCHAR,
+    artist_latitude FLOAT,
+    artist_longitude FLOAT,
+    artist_location VARCHAR,
+    artist_name VARCHAR,
+    song_id VARCHAR,
+    title VARCHAR,
+    duration FLOAT,
+    year FLOAT
+);
 """)
 
 songplay_table_create = ("""
+CREATE TABLE IF NOT EXISTS songplays (
+    songplay_id INT IDENTITY (1, 1) PRIMARY KEY, 
+    start_time TIMESTAMP,
+    user_id INT, 
+    level VARCHAR, 
+    song_id VARCHAR, 
+    artist_id VARCHAR, 
+    session_id INT, 
+    location VARCHAR, 
+    user_agent VARCHAR
+)
+DISTSTYLE KEY
+DISTKEY (start_time)
+SORTKEY (start_time);
 """)
 
 user_table_create = ("""
+CREATE TABLE IF NOT EXISTS users (
+    user_id INT PRIMARY KEY,
+    first_name VARCHAR,
+    last_name VARCHAR,
+    gender VARCHAR,
+    level VARCHAR
+)
+SORTKEY (user_id);
 """)
 
 song_table_create = ("""
+CREATE TABLE IF NOT EXISTS songs (
+    song_id VARCHAR PRIMARY KEY, 
+    title VARCHAR,
+    artist_id VARCHAR,
+    year INT,
+    duration FLOAT
+)
+SORTKEY (song_id);
 """)
 
 artist_table_create = ("""
+CREATE TABLE IF NOT EXISTS artists (
+    artist_id VARCHAR PRIMARY KEY, 
+    name VARCHAR,
+    location VARCHAR,
+    latitude FLOAT,
+    longitude FLOAT
+)
+SORTKEY (artist_id);
 """)
 
 time_table_create = ("""
+CREATE TABLE IF NOT EXISTS time (
+    start_time TIMESTAMP PRIMARY KEY, 
+    hour INT,
+    day INT,
+    week INT,
+    month INT,
+    year INT,
+    weekday INT
+)
+DISTSTYLE KEY
+DISTKEY (start_time)
+SORTKEY (start_time);
 """)
+
 
 # STAGING TABLES
 
 staging_events_copy = ("""
-""").format()
+COPY staging_events
+FROM {data} iam_role {role_arn}
+FORMAT AS json {json};
+""").format(data=config['S3']['LOG_DATA'], role_arn=config['IAM_ROLE']['ARN'], json=config['S3']['LOG_JSONPATH'])
 
 staging_songs_copy = ("""
-""").format()
+COPY staging_songs
+FROM {data} iam_role {role_arn}
+FORMAT AS json 'auto';
+""").format(data=config['S3']['SONG_DATA'], role_arn=config['IAM_ROLE']['ARN'])
+
 
 # FINAL TABLES
 
 songplay_table_insert = ("""
+INSERT INTO songplays (start_time, user_id, level, song_id, artist_id, session_id, location, user_agent)
+SELECT DISTINCT 
+    -- FROM: https://stackoverflow.com/questions/39815425/how-to-convert-epoch-to-datetime-redshift
+    TIMESTAMP 'epoch' + (e.ts / 1000) * INTERVAL '1 second',
+    e.userId, 
+    e.level, 
+    s.song_id, 
+    s.artist_id, 
+    e.sessionId, 
+    e.location, 
+    e.userAgent
+FROM staging_events e
+INNER JOIN staging_songs s
+    ON e.song = s.title 
+        AND e.artist = s.artist_name 
+        AND e.page = 'NextSong'
 """)
 
 user_table_insert = ("""
+INSERT INTO users (user_id, first_name, last_name, gender, level)
+SELECT DISTINCT userId, firstName, lastName, gender, level
+FROM staging_events
+WHERE userId IS NOT NULL
+    AND page = 'NextSong';
 """)
 
 song_table_insert = ("""
+INSERT INTO songs (song_id, title, artist_id, year, duration)
+SELECT DISTINCT song_id, title, artist_id, year, duration
+FROM staging_songs
+WHERE song_id IS NOT NULL;
 """)
 
 artist_table_insert = ("""
+INSERT INTO artists (artist_id, name, location, latitude, longitude)
+SELECT DISTINCT artist_id, artist_name, artist_location, artist_latitude, artist_longitude
+FROM staging_songs
+WHERE artist_id IS NOT NULL;
 """)
 
 time_table_insert = ("""
+INSERT INTO time (start_time, hour, day, week, month, year, weekday)
+SELECT DISTINCT 
+    TIMESTAMP 'epoch' + (ts/1000) * INTERVAL '1 second' as start_time,
+    EXTRACT(hour FROM start_time),
+    EXTRACT(day FROM start_time),
+    EXTRACT(week FROM start_time),
+    EXTRACT(month FROM start_time),
+    EXTRACT(year FROM start_time),
+    EXTRACT(dayofweek FROM start_time)
+FROM staging_events;
 """)
+
 
 # QUERY LISTS
 
